7d88e2e3fd6694be57ab026bcfd169046a67bb83
lrz2662061323 <74164642+lrz2662061323@users.noreply.github.com>
2022-08-25 21:16:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ def get_weather():
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
 
+"""
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
+"""
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
